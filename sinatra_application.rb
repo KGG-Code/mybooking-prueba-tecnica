6427d04358dabd79dc1d1e087bd7cf7b5f45fafc
meuk <dev1@metodo10.com>
2025-09-12 00:37:49 +0200

    add API error handling

--- sinatra_application.rb
+++ sinatra_application.rb
@@ -7,6 +7,10 @@ module Sinatra
       set :root, File.expand_path('..', __FILE__)
       set :views, File.join(root, 'app/views')
       set :public_folder, File.join(root, 'app/assets')
+      
+      # Disable Sinatra's error handling to let our middleware handle it
+      set :show_exceptions, false
+      set :raise_errors, true
     end
 
     # Include the routes defined in the controller
